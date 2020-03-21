f3418c526afffbbb50f4e2a48b52f80fcfc527af
Gary Xu <xjs1997524@gmail.com>
2020-03-21 23:12:04 +0800

    fully accomplish the function of rest api - new function can put certain comments with a comments id and improve its comment or author

--- app.js
+++ app.js
@@ -18,9 +18,11 @@ const url = 'mongodb://localhost:27017/conFusion';
 
 const connect = mongoose.connect(url);
 
-connect.then( function (db){
+connect.then(function (db) {
   console.log('connect to the server');
-}, function(err){ console.log(err);})
+}, function (err) {
+  console.log(err);
+})
 // -- end of mongodb part -- 
 
 var app = express();
@@ -31,7 +33,9 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -42,12 +46,12 @@ app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- models/dishes.js
+++ models/dishes.js
@@ -6,67 +6,67 @@ require('mongoose-currency').loadType(mongoose);
 const Currency = mongoose.Types.Currency;
 
 const commentSchema = new Schema({
-    rating:{
-        type:Number,
-        min:1,
-        max:5,
-        required:true
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
     },
-    comment:{
-        type:String,
-        required:true
+    comment: {
+        type: String,
+        required: true
     },
-    author:{
-        type:String,
-        required:true
+    author: {
+        type: String,
+        required: true
     }
-},{
-    timestamps:true
+}, {
+    timestamps: true
 });
 
 const dishSchema = new Schema({
-    name:{
+    name: {
         type: String,
         required: true,
         unique: true
     },
-    description:{
+    description: {
         type: String,
-        required :  true
-    },
-    image:{
-        type : String,
         required: true
     },
-    category:{
-        type:String,
-        required:true
+    image: {
+        type: String,
+        required: true
     },
-    label:{
-        type:String,
-        default:''
+    category: {
+        type: String,
+        required: true
     },
-    price:{
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
         // 这里用到了 之前导入的 currency
-        type:Currency,
-        required:true,
+        type: Currency,
+        required: true,
         min: 0
     },
-    feature:{
-        type:Boolean,
-        default:false
+    feature: {
+        type: Boolean,
+        default: false
     },
     // -- sub-document --
     // save all the comments as array
-    comments: [ commentSchema ]
-},{
+    comments: [commentSchema]
+}, {
     // 自动把所有的timestamps加入到所有的schema类别中
-    timestamps:true
+    timestamps: true
 });
 
 // 创建一个export用的model
 // 'Dish'会被map into a collection 'Dishes'
 //  -她会自己把你弄成复述
-var Dishes = mongoose.model('Dish',dishSchema);
+var Dishes = mongoose.model('Dish', dishSchema);
 
 module.exports = Dishes;
