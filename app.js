eeb164eac94d47141b90c1546029889cfcabe379
Gary Xu <xjs1997524@gmail.com>
2020-03-23 21:50:08 +0800

    use cookie to fullfill the function [cookie - authentic info]

--- app.js
+++ app.js
@@ -41,40 +41,57 @@ app.use(
 		extended: false
 	})
 );
-app.use(cookieParser());
+// * signed-cookie : 需要添加'String'
+app.use(cookieParser('12345-67890-09876-54321'));
 
 // ! --- start of authentic part ---
 function auth(req, res, next) {
-	console.log(req.headers);
-	var authHeader = req.headers.authorization;
-	if (!authHeader) {
-		var err = new Error('You are not authenticated!');
-		res.setHeader('WWW-Authenticate', 'Basic');
-		err.status = 401;
-		return next(err);
-	}
-	// * 1. Buffer - enable to split value
-	// * 2. 'base64' - encoding type
-	// * 3. split(' ') - 把string通过' '(空格)分成几个部分， 放入一个array重
-	// *        因为这个string是用一个space隔开的，所以[0] -> basic, [1] -> username+password
-	// * 4. toString().split(":") - username与password通过一个 ; 隔开， 把得到的密码再split到一个array里
-	// * 5. auth - 一个array， 含有[0]-username,[1] - password
-	// ! Buffer.from - 翻译二进制文字 Buffer.from(xx,'base64'); 'base64'->格式
-	var auth = new Buffer.from(authHeader.split(' ')[1], 'base64')
-		.toString()
-		// !用的是':'而不是分号！
-		.split(':');
+	console.log(req.signedCookies);
+	// * if the user have not been authenticated yet
+	if (!req.signedCookies.uid) {
+		var authHeader = req.headers.authorization;
+		if (!authHeader) {
+			var err = new Error('You are not authenticated!');
+			res.setHeader('WWW-Authenticate', 'Basic');
+			err.status = 401;
+			return next(err);
+		} else {
+			// * 1. Buffer - enable to split value
+			// * 2. 'base64' - encoding type
+			// * 3. split(' ') - 把string通过' '(空格)分成几个部分， 放入一个array重
+			// *        因为这个string是用一个space隔开的，所以[0] -> basic, [1] -> username+password
+			// * 4. toString().split(":") - username与password通过一个 ; 隔开， 把得到的密码再split到一个array里
+			// * 5. auth - 一个array， 含有[0]-username,[1] - password
+			// ! Buffer.from - 翻译二进制文字 Buffer.from(xx,'base64'); 'base64'->格式
+			var auth = new Buffer.from(authHeader.split(' ')[1], 'base64')
+				.toString()
+				// !用的是':'而不是分号！
+				.split(':');
 
-	var username = auth[0];
-	var password = auth[1];
+			var username = auth[0];
+			var password = auth[1];
 
-	if (username === 'admin' && password === 'password') {
-		next();
+			if (username === 'admin' && password === 'password') {
+				// * use the res.cookie here
+				// 设置一个新的cookie
+				res.cookie('uid', 'admin', { signed: true });
+				next();
+			} else {
+				var err = new Error('You are not authenticated!');
+				res.setHeader('WWW-Authenticate', 'Basic');
+				err.status = 401;
+				return next(err);
+			}
+		}
 	} else {
-		var err = new Error('You are not authenticated!');
-		res.setHeader('WWW-Authenticate', 'Basic');
-		err.status = 401;
-		return next(err);
+		if (req.signedCookies.uid == 'admin') {
+			next();
+		} else {
+			// *一般不会有这种情况: 含有cookie但是是不合格的cookie，不让访问
+			var err = new Error('You are not authenticated');
+			err.status = 401;
+			return next(err);
+		}
 	}
 }
 
